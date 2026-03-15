c9640b2dff18b0c0115463204639db780456cb99
mvtt1803 <maivutrungtin@gmail.com>
2026-03-15 15:37:51 +0700

    initial wishlistformo

---
+++ script.js
@@ -0,0 +1,55 @@
+const SUPABASE_URL = "https://eijqtqlvdtggyvqhchya.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpanF0cWx2ZHRnZ3l2cWhjaHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NjE1MDksImV4cCI6MjA4OTAzNzUwOX0.qAdJDwMLgqf7baaSd_BrCKMUl_j47OkqAguEK4k8p6s"
+
+const client = supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
+
+const list = document.getElementById("list")
+const form = document.getElementById("form")
+
+async function loadItems(){
+
+const {data} = await client
+.from("wishlist")
+.select("*")
+.order("desire",{ascending:false})
+
+list.innerHTML=""
+
+data.forEach(item=>{
+
+const div=document.createElement("div")
+div.className="item"
+
+div.innerHTML=`
+<strong>${item.name}</strong><br>
+${item.price ? item.price + " k VND" : ""}<br>
+${"⭐".repeat(item.desire)}<br>
+<a href="${item.link}" target="_blank">Open link</a>
+`
+
+list.appendChild(div)
+
+})
+
+}
+
+form.addEventListener("submit",async e=>{
+
+e.preventDefault()
+
+const item={
+name:document.getElementById("name").value,
+price:parseInt(document.getElementById("price").value)||null,
+link:document.getElementById("link").value,
+desire:parseInt(document.getElementById("desire").value)
+}
+
+await client.from("wishlist").insert([item])
+
+form.reset()
+
+loadItems()
+
+})
+
+loadItems()
