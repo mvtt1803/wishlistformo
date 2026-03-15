b350086fe9ca4d3c9cf1a0054645d5b8e61f2729
mvtt1803 <maivutrungtin@gmail.com>
2026-03-15 15:56:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const SUPABASE_URL = "https://eijqtqlvdtggyvqhchya.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpanF0cWx2ZHRnZ3l2cWhjaHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NjE1MDksImV4cCI6MjA4OTAzNzUwOX0.qAdJDwMLgqf7baaSd_BrCKMUl_j47OkqAguEK4k8p6s"
 
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+const { createClient } = window.supabase
+const client = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 const list = document.getElementById("list")
 const form = document.getElementById("form")
@@ -34,7 +35,7 @@ data.forEach(item => {
   div.innerHTML = `
     <strong>${item.name}</strong><br>
     ${item.price ? item.price + " k VND" : ""}<br>
-    ${"⭐".repeat(item.desire)}<br>
+    ${"⭐".repeat(item.desire || 1)}<br>
     <a href="${item.link}" target="_blank">Open link</a>
   `
 
